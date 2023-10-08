fa7cc828d72099813b64752486e660334a17877c
meet2sgr <sagarpatil0901@gmail.com>
2023-10-09 00:55:41 +0530

    Add project files.

---
+++ CallCustomerAPI/Controllers/CustomersController.cs
@@ -0,0 +1,91 @@
+﻿using CallCustomerAPI.Models;
+using CallCustomerAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CallCustomerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly ICustomerService _customerService;
+        public CustomersController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
+        {
+            try
+            {
+                var customers = await _customerService.GetCustomersAsync();
+                return Ok(customers);
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, "An error occurred while fetching a customer list");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomerById(string id)
+        {
+            try
+            {
+                var customers = await _customerService.GetCustomerByIdAsync(id);
+                return Ok(customers);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "An error occurred while fetching a customer");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Customer>> CreateCustomer([FromBody] Customer customer)
+        {
+            try
+            {
+                var createdCustomer = await _customerService.CreateCustomerAsync(customer);
+                return CreatedAtAction(nameof(GetCustomerById), new { id = customer.Id }, createdCustomer);
+            }
+            catch
+            {
+                return StatusCode(500, "An error occurred while creating a customer");
+            }
+        }
+
+        [HttpPost("{id}")]
+        public async Task<IActionResult> EditCustomer(string id, [FromBody] Customer customer)
+        {
+            try
+            {
+                var editedCustomer = await _customerService.EditCustomerAsync(id, customer);
+                return CreatedAtAction(nameof(GetCustomerById), new { id = customer.Id }, editedCustomer);
+            }
+            catch
+            {
+                return StatusCode(500, "An error occurred while updating a customer");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCustomer(string id)
+        {
+            try
+            {
+                await _customerService.DeleteCustomerAsync(id);
+                return NoContent();
+            }
+            catch
+            {
+                return StatusCode(500, "An error occurred while deleting a customer");
+            }
+        }
+    }
+}
---
+++ CallCustomerAPI/Services/CustomerService.cs
@@ -0,0 +1,63 @@
+﻿using CallCustomerAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CallCustomerAPI.Services
+{
+    public interface ICustomerService
+    {
+        Task<IEnumerable<Customer>> GetCustomersAsync();
+        Task<Customer> CreateCustomerAsync(Customer customer);
+        Task<Customer> EditCustomerAsync(string id, Customer customer);
+        Task<Customer> GetCustomerByIdAsync(string id);
+        Task DeleteCustomerAsync(string id);
+    }
+    public class CustomerService : ICustomerService
+    {
+        private readonly HttpClient _httpClient;
+
+        public CustomerService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient("InvoiceApi");
+        }
+
+        public async Task<IEnumerable<Customer>> GetCustomersAsync()
+        {
+            var response = await _httpClient.GetAsync("Customers");
+            response.EnsureSuccessStatusCode();
+            var customers = await response.Content.ReadAsAsync<IEnumerable<Customer>>();
+            return customers;
+        }
+
+        public async Task<Customer> GetCustomerByIdAsync(string id)
+        {
+            var response = await _httpClient.GetAsync($"Customer/{id}");
+            response.EnsureSuccessStatusCode();
+            var customer = await response.Content.ReadAsAsync<Customer>();
+            return customer;
+        }
+
+        public async Task<Customer> CreateCustomerAsync(Customer customer)
+        {
+            var response = await _httpClient.PostAsJsonAsync("Customer", customer);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsAsync<Customer>();
+        }
+
+        public async Task<Customer> EditCustomerAsync(string id, Customer customer)
+        {
+            var response = await _httpClient.PostAsJsonAsync($"Customer/{id}", customer);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsAsync<Customer>();
+        }
+
+        public async Task DeleteCustomerAsync(string id)
+        {
+            var response = await _httpClient.DeleteAsync($"Customer/{id}");
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
